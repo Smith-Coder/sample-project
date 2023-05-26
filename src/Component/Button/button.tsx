4a94e256ca4f2d38d305d3534f53ede7210f34a9
Smith C D <Smith.CD@trivium-esolutions.com>
2023-05-26 16:30:16 +0530

    update

--- src/Component/Button/button.tsx
+++ src/Component/Button/button.tsx
@@ -7,13 +7,13 @@ function button() {
     let text = "Alert"
     const [showComponent, setComponent] = useState(false);
 
-
     return (
         <div>
             <button
                 type="button"
                 className="btn btn-primary"
-                onClick={() => setComponent(true)}>{text}</button>
+                onClick={() => showComponent == false ? setComponent(true) : setComponent(false)}
+            >{text}</button>
             {showComponent && <Alert>This is an alert</Alert>}
         </div>
     )
